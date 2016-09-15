09e482dc48e57936b0c78295bd80f3a2dc81d006
Jan Nicklas <j.nicklas@me.com>
2016-09-15 16:56:41 +0200

    Turn off conflicting namics eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
-module.exports = require('@namics/eslint-config/configurations/es6-node.js');
-module.exports.parser = 'babel-eslint';
-module.exports.plugins = ['import'];
+module.exports = {
+  'extends': '@namics/eslint-config/configurations/es6-node.js',
+  'parser': 'babel-eslint',
+  'plugins': ['import'],
+  'rules': {
+	  'arrow-parens': 0, // turned of because of false positives when using await shorthands
+	  'complexity': 0 // turned of because because we can't reduce the complexity of the constructor
+  }
+};
